7809c981b8cb6f66f240a0cb61b213b7f820a985
NsGun <pattonsayd@gmail.com>
2023-03-27 01:22:00 +0400

    Adding bloc SM

--- lib/domain/game.dart
+++ lib/domain/game.dart
@@ -2,86 +2,136 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:capitals_quiz/data/data.dart';
 import 'package:capitals_quiz/domain/game_items_logic.dart';
 import 'package:capitals_quiz/domain/models.dart';
 import 'package:capitals_quiz/domain/palette.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+
+abstract class GameEvent {
+  const GameEvent();
+}
+
+class OnStartGameEvent implements GameEvent {
+  const OnStartGameEvent();
+}
+
+class OnResetGameEvent implements GameEvent {
+  const OnResetGameEvent();
+}
+
+class OnGuessGameEvent implements GameEvent {
+  final int index;
+  final bool isTrue;
+  const OnGuessGameEvent({
+    required this.index,
+    required this.isTrue,
+  });
+}
 
 class GameState {
   final int score;
   final int topScore;
+  final List<Country> countries;
 
-  const GameState({required this.score, required this.topScore});
+  const GameState({
+    required this.score,
+    required this.topScore,
+    required this.countries,
+  });
 
-  static const GameState empty = GameState(score: 0, topScore: 1);
+  static const GameState empty =
+      GameState(score: 0, topScore: 1, countries: []);
 
   double get progress => max(0, score) / topScore;
 
   GameState copyWith({
     int? score,
     int? topScore,
+    List<Country>? countries,
   }) {
     return GameState(
       score: score ?? this.score,
       topScore: topScore ?? this.topScore,
+      countries: countries ?? this.countries,
     );
   }
 }
 
-class GameLogic extends Cubit<GameState> {
+class GameLogic extends Bloc<GameEvent, GameState> {
   final Random _random;
   final Api _api;
   final Assets _assets;
   final PaletteLogic _palette;
   final GameItemsLogic _gameItemsLogic;
+  final int countryLimit;
 
   GameLogic(
     this._random,
     this._api,
     this._assets,
     this._palette,
-    this._gameItemsLogic,
-  ) : super(GameState.empty);
+    this._gameItemsLogic, {
+    this.countryLimit = GameLogic.defaultCountryLimit,
+  }) : super(GameState.empty) {
+    on<OnStartGameEvent>(_onStartGame);
+    on<OnGuessGameEvent>(_onGuess);
+    on<OnResetGameEvent>(_onReset);
+  }
 
   static const _successGuess = 3;
   static const _successFake = 1;
   static const _fail = -1;
-  static const countryLimit = 20;
+  static const defaultCountryLimit = 20;
 
-  List<Country>? _allCountries;
-
-  Future<void> onStartGame() async {
+  Future<void> _onStartGame(
+    OnStartGameEvent event,
+    Emitter<GameState> emit,
+  ) async {
+    var resultState = state;
     try {
-      var countries = _allCountries ??= await _api.fetchCountries();
-      countries.shuffle(_random);
-      countries = countries.sublist(0, countryLimit);
+      var countries = await _api.fetchCountries();
       countries = _mergeCountryWithImages(countries);
-      _prepareItems(countries);
+      resultState = resultState.copyWith(countries: [...countries]);
+      final limitedCountries = _getCountriesForNewGame(countries);
+      resultState = _prepareItems(resultState, limitedCountries);
     } catch (e) {
       debugPrint(e.toString());
     }
-    _onUpdatePalette();
+    await _updatePalette();
+    emit(resultState);
   }
 
-  void _prepareItems(List<Country> countries) {
+  List<Country> _getCountriesForNewGame(List<Country> countries) {
+    final copyToShuffle = [...countries];
+    copyToShuffle.shuffle(_random);
+    return copyToShuffle.sublist(0, countryLimit);
+  }
+
+  GameState _prepareItems(GameState state, List<Country> countries) {
     _gameItemsLogic.updateGameItems(countries);
     final originals = _gameItemsLogic.state.originalLength;
     final fakes = _gameItemsLogic.state.fakeLength;
     final topScore = originals * _successGuess + fakes * _successFake;
-    _updateTopScore(topScore);
+    return _updateTopScore(state, topScore);
   }
 
-  void _onUpdatePalette() => _palette.updatePalette(
+  Future<void> _updatePalette() async => _palette.updatePalette(
       _gameItemsLogic.state.current.image, _gameItemsLogic.state.next?.image);
 
-  void _updateTopScore(int topScore) =>
-      emit(state.copyWith(topScore: topScore));
+  GameState _updateTopScore(GameState state, int topScore) =>
+      state.copyWith(topScore: topScore);
 
-  void _updateScore(int score) => emit(state.copyWith(score: score));
+  GameState _updateScore(GameState state, int score) =>
+      state.copyWith(score: score);
 
-  void onGuess(int index, bool isTrue) {
+  Future<void> _onGuess(
+    OnGuessGameEvent event,
+    Emitter<GameState> emit,
+  ) async {
+    final index = event.index;
+    final isTrue = event.isTrue;
     final isActuallyTrue = _gameItemsLogic.state.isCurrentTrue;
     int scoreUpdate = 0;
 
@@ -97,15 +147,17 @@ class GameLogic extends Cubit<GameState> {
       scoreUpdate = _fail;
     }
 
-    _updateScore(state.score + scoreUpdate);
+    final resultState = _updateScore(state, state.score + scoreUpdate);
     _gameItemsLogic.updateCurrent(index);
 
     if (!_gameItemsLogic.state.isCompleted) {
-      _onUpdatePalette();
+      await _updatePalette();
     }
 
     debugPrint(
         'Score: ${state.score}/${state.topScore}. Card: ${_gameItemsLogic.state.currentIndex}/${_gameItemsLogic.state.gameItems.length}');
+
+    emit(resultState);
   }
 
   List<Country> _mergeCountryWithImages(List<Country> countries) {
@@ -125,10 +177,17 @@ class GameLogic extends Cubit<GameState> {
     }).toList();
   }
 
-  Future<void> onReset() async {
-    _updateScore(0);
-    _updateTopScore(1);
+  void _onReset(
+    OnResetGameEvent event,
+    Emitter<GameState> emit,
+  ) {
     _gameItemsLogic.reset();
-    await onStartGame();
+    final limitedCountries = _getCountriesForNewGame(state.countries);
+    final newState = _prepareItems(
+      state.copyWith(score: 0),
+      limitedCountries,
+    );
+
+    emit(newState);
   }
 }
--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 class GameItems {
   final Country original;
@@ -11,6 +12,18 @@ class GameItems {
   String? get capital => fake?.capital ?? original.capital;
 
   ImageProvider get image => original.image;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is GameItems &&
+        other.original == original &&
+        other.fake == fake;
+  }
+
+  @override
+  int get hashCode => original.hashCode ^ fake.hashCode;
 }
 
 class Country {
@@ -27,6 +40,25 @@ class Country {
   });
 
   ImageProvider get image => NetworkImage('${imageUrls[index]}?w=600');
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Country &&
+        other.name == name &&
+        other.capital == capital &&
+        listEquals(other.imageUrls, imageUrls) &&
+        other.index == index;
+  }
+
+  @override
+  int get hashCode {
+    return name.hashCode ^
+        capital.hashCode ^
+        imageUrls.hashCode ^
+        index.hashCode;
+  }
 }
 
 class ColorPair {
